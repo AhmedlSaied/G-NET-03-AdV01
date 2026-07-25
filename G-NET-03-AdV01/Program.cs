9355c59c0c50ec5709f32198afa57ce43b4ecad9
AhmedlSaied <kledkled.17@gmail.com>
2026-07-25 07:03:01 +0300

    SUPPORTING TYPES & INTERFACES

--- G-NET-03-AdV01/Program.cs
+++ G-NET-03-AdV01/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdvancedCSharpGenerics
+{
+    #region SUPPORTING TYPES & INTERFACES
+
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+
+    public class BaseEntity : IEntity
+    {
+        public int Id { get; set; }
+    }
+
+    public class User : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public User() { } 
+    }
+
+    #endregion
+}
